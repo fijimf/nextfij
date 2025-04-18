4f279c3e2d983b132df004c9827d187bb86428f2
jamesfrohnhofer <fijimf@gmail.com>
2025-04-18 10:16:52 -0400

    Add remote image patterns to Next.js configuration for ESPN team logos, enabling optimized image rendering from external sources.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,25 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'a.espncdn.com',
+        port: '',
+        pathname: '/guid/**',
+        search: '',
+      },
+      // https://a.espncdn.com/i/teamlogos/ncaa_conf/500/big_east.png
+      {
+        protocol: 'https',
+        hostname: 'a.espncdn.com',
+        port: '',
+        pathname: '/i/teamlogos/ncaa_conf/**',
+        search: '',
+      },
+    ],
+  },
   /* config options here */
 };
 
