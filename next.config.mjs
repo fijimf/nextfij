ff3dd318e32827d929cd7d6a54c7b3b5a9750375
jamesfrohnhofer <fijimf@gmail.com>
2025-05-15 23:49:14 -0400

    Add remote pattern for NCAA team logos in Next.js configuration
    - Included a new remote pattern for loading NCAA team logos from ESPN, enhancing image handling capabilities in the application.

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   output: 'standalone',
   images: {
     remotePatterns: [
+      // (https://a.espncdn.com/i/teamlogos/ncaa/500/239.png) 
+      {
+        protocol: 'https',
+        hostname: 'a.espncdn.com',
+        port: '',
+        pathname: '/i/teamlogos/ncaa/**',
+        search: '',
+      },
       {
         protocol: 'https',
         hostname: 'a.espncdn.com',
